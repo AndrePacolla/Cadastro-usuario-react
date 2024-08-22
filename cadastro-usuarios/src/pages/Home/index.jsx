335e8db21a85da6926465a341d2b5a61644ae541
AndrePacolla <andreluizzpacolla@outlook.com>
2024-08-22 17:38:57 -0300

    adc img svg trash

--- cadastro-usuarios/src/pages/Home/index.jsx
+++ cadastro-usuarios/src/pages/Home/index.jsx
@@ -1,11 +1,30 @@
 import './style.css'
-
+import Trash from '../../assets/trash.svg'
 function Home() {
 
   return (
     
-    <div>
-      <h1>Olá React</h1>
+    <div className='container'>
+      <form>
+      <h1>Cadastro de Usuário</h1>
+      <input name='nome' type='text'/>
+      <input name='idade' type='number'/>
+      <input name='e-mail' type='email'/>
+      <button type='button'>Cadastrar</button>
+      </form>
+
+      <div>
+        <div>
+          <p>Nome:</p>
+          <p>Idade:</p>]
+          <p>E-mail</p>
+        </div>
+
+        <button>
+          <img src={Trash}/>
+        </button>
+      </div>
+
     </div>
   )
 }
